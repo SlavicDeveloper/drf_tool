890ffb845283717bb818ea712b484f1de24ef761
SlavicDeveloper <mde1533@mail.ru>
2022-01-08 22:29:55 +0300

    "lesson_1. Model and registration"

---
+++ userapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class User(models.Model):
+   first_name = models.CharField(max_length=64, blank=False)
+   last_name = models.CharField(max_length=64, blank=False)
+   birthday_year = models.PositiveIntegerField()
+   email = models.EmailField(unique=True, blank=False)
+
+
