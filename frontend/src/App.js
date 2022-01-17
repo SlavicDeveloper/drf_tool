aeb5c2d2212b5b2330996d015253a6b47130a084
SlavicDeveloper <mde1533@mail.ru>
2022-01-17 03:14:27 +0300

    "Lesson_2. Users, menu, footer"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
+import Footer from './components/footer.js';
+import Menubar from './components/menu.js';
 import './App.css';
+import UserList from './components/Users.js'
 
 class App extends React.Component {
    constructor(props) {
@@ -9,11 +13,30 @@ class App extends React.Component {
            'users': []
        }
    }
+// url:"http://127.0.0.1:8000/api/users"
+    componentDidMount() {
+        axios.get("http://127.0.0.1:8000/api/users/")
+            .then(response => {
+                const users = response.data
+                    this.setState(
+                    {
+                        "users":users
+                    }
+                )
+            }).catch(error => console.log(error))
+
+    }
 
    render () {
        return (
            <div>
-               Main App
+               <header>
+                    <Menubar />
+               </header>
+               <main>
+                    <UserList users={this.state.users} / >
+               </main>
+               <Footer />
            </div>
        )
    }
---
+++ frontend/src/components/menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Menubar() {
+    return (
+        <menu>
+          <li> Пробный проект </li>
+           <li> В разработке </li>
+        </menu>
+    );
+}
+
+
+export default Menubar
