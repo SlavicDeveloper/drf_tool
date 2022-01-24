1b7b329589ce834b5fd728e011f2c4212b507a23
SlavicDeveloper <mde1533@mail.ru>
2022-01-24 17:55:53 +0300

    "Lesson_4. Users using ViewSet"

--- library_todo/urls.py
+++ library_todo/urls.py
@@ -16,17 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
 from userapp.views import UserModelViewSet
 from todoapp.views import ProjectModelViewSet, TODOModelViewSet
 
 router = DefaultRouter()
-router.register("users", UserModelViewSet)
-router.register("projects", ProjectModelViewSet)
-router.register("todos", TODOModelViewSet)
+router.register("modified_users", UserModelViewSet, basename="users")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(router.urls)),
+    path("viewsets/", include(router.urls)),
 ]
