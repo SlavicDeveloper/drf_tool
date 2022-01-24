49317f32b3ae2dfae4b62c0b1a912ba3febc7650
SlavicDeveloper <mde1533@mail.ru>
2022-01-24 20:28:37 +0300

    "Lesson_4. Filtering, pagination and ModelViewSet" of TODO

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project
@@ -10,15 +11,34 @@ from rest_framework.pagination import LimitOffsetPagination
 from .filters import ProjectFilter
 
 
-class ProjectPageNumberPagination(LimitOffsetPagination):
+class ProjectLimitOffSetPagination(LimitOffsetPagination):
     default_limit = 10
+
+
+class TODOLimitOffSetPagination(LimitOffsetPagination):
+    default_limit = 20
+
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
-    pagination_class = ProjectPageNumberPagination
+    pagination_class = ProjectLimitOffSetPagination
     filter_class = ProjectFilter
+
 
 class TODOModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = TODOLimitOffSetPagination
+    filterset_fields = ['project_name', 'text', 'creation_date', 'update_date',
+                        'users_checklist_author', 'status']
+
+    def destroy(self, request, pk=None):
+        try:
+            elem = self.get_object()
+            elem.status = False
+            elem.save()
+        except:
+            return Response(status="404_not_found")
+        else:
+            return Response(status="404_not_found")
