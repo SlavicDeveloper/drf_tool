43ddc69d0f28e55eddcca9a2908db99b6b2cdc5a
SlavicDeveloper <mde1533@mail.ru>
2022-02-04 20:27:06 +0300

    "Lesson_8. APIClient tests"

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,3 +1,24 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from .views import ProjectModelViewSet, TODOModelViewSet
+from .models import Project, TODO
+
 
 # Create your tests here.
+
+class TestProjectsAndTodosViewsets(TestCase):  # OK, пользователь не авторизован
+    def test_get_project(self):
+        client = APIClient()
+        response = client.get("http://127.0.0.1:8000/api/modified_projects/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_project_auth(self):  # OK, пользователь авторизирован, и проекты получены
+        client = APIClient()
+        admin = User.objects.create_superuser("admin", "admin@mail.ru", "admin123456")
+        client.force_authenticate(admin)
+        response = client.get("http://127.0.0.1:8000/api/modified_projects/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        client.logout()
--- userapp/tests.py
+++ userapp/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 import json
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate
-# from mixer.backend.django import mixer
 from django.contrib.auth.models import User
 from .views import UserModelViewSet
 
