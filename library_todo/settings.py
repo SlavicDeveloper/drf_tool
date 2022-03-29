11a06e72f48beedff1ebc21ef4f19db093b7f06e
SlavicDeveloper <mde1533@mail.ru>
2022-03-30 00:58:42 +0300

    Docker-compose file for adminer

--- library_todo/settings.py
+++ library_todo/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = "library_todo.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drf_tool',
+        'USER': 'dante',
+        'PASSWORD': 'dante123456',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
